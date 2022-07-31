85a7762a30e074f20958c750b301bda2f91b66cd
thijsBoet <m.boet@chello.nl>
2022-07-31 14:26:36 +0200

    finished store page ui

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { Col, Row } from 'react-bootstrap';
+import StoreItem from '../components/StoreItem';
+import storeItems from '../data/items.json';
 
 const Store = () => {
 	return (
-		<div>Store</div>
-	)
-}
+		<>
+			<div>
+				<h1>Store</h1>
+			</div>
+			<Row lg={3} md={2} xs={1} className='g-3'>
+				{storeItems.map(item => (
+					<Col key={item.id}>
+						<StoreItem {...item} />
+					</Col>
+				))}
+			</Row>
+		</>
+	);
+};
 
-export default Store
+export default Store;
