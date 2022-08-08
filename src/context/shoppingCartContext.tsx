89eae235242cd43e3cde31c70089b91e766a0d32
Matthijs Boet <mboet@client13.ict.fqdn>
2022-08-08 14:14:47 +0200

    finished shoppingcart project

--- src/context/shoppingCartContext.tsx
+++ src/context/shoppingCartContext.tsx
@@ -1,19 +1,25 @@
-import { createContext, ReactNode, useContext, useState } from "react";
+import { createContext, ReactNode, useContext, useState } from 'react';
+import ShoppingCart from '../components/ShoppingCart';
+import { useLocalStorage } from '../hooks/useLocalStorage';
 
 type ShoppingCartProviderProps = {
 	children: ReactNode;
-}
+};
 
 type CartItem = {
 	id: number;
 	quantity: number;
-}
+};
 
 type ShoppingCartContext = {
+	openCart: () => void;
+	closeCart: () => void;
 	getItemQuantity: (id: number) => number;
 	increaseCartQuantity: (id: number) => void;
 	decreaseCartQuantity: (id: number) => void;
 	removeFromCart: (id: number) => void;
+	cartQuantity: number;
+	cartItems: CartItem[];
 };
 
 const ShoppingCartContext = createContext({} as ShoppingCartContext);
@@ -23,7 +29,15 @@ export function useShoppingCart() {
 }
 
 export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
-	const [cartItems, setCartItems] = useState<CartItem[]>([])
+	const [cartItems, setCartItems] = useLocalStorage<CartItem[]>("shopping-cart", []);
+	const [isOpen, setIsOpen] = useState(false);
+
+	const cartQuantity = cartItems.reduce(
+		(quantity, item) => item.quantity + quantity,
+		0
+	);
+	const openCart = () => setIsOpen(true);
+	const closeCart = () => setIsOpen(false);
 
 	function getItemQuantity(id: number) {
 		return cartItems.find(item => item.id === id)?.quantity || 0;
@@ -42,7 +56,7 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
 					}
 				});
 			}
-		})
+		});
 	}
 
 	function decreaseCartQuantity(id: number) {
@@ -72,9 +86,14 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
 				increaseCartQuantity,
 				decreaseCartQuantity,
 				removeFromCart,
+				openCart,
+				closeCart,
+				cartItems,
+				cartQuantity,
 			}}
 		>
 			{children}
+			<ShoppingCart isOpen={isOpen} />
 		</ShoppingCartContext.Provider>
 	);
 }
